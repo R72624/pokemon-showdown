8ef91fcb08b75b2e4f4affa59b10370036c724d2
R72624 <rch1234567@outlook.com>
2024-07-26 15:44:29 -0400

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -5651,4 +5651,17 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		rating: 3,
 		num: -4,
 	},
+	haste: {
+		onModifyAccuracyPriority: 10,
+		onModifyAccuracy(accuracy, target, source, move) {
+			if (move.category === 'Status' && typeof accuracy === 'number') {
+				this.debug('Wonder Skin - setting accuracy to 50');
+				return 50;
+			}
+		},
+		flags: {breakable: 1},
+		name: "Haste",
+		rating: 2,
+		num: -50,
+	},
 };
