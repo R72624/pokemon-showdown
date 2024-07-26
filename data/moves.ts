52df7250d3ffba7e2a1c3e1e6912bc34a04ab340
R72624 <rch1234567@outlook.com>
2024-07-26 14:49:58 -0400

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -22090,10 +22090,5 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		type: "Electric",
 		contestType: "Cool",
 	},
-	placeholder: {
-		name: "Place Holder",
-		desc: "Has a 30% chance to make the target flinch.",
-		shortDesc: "30% chance to make the target flinch.",
-	},
 };
 };
